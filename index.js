83470e36096b0674ff3b8ee8c6add466b0262494
Shapna <shapna088@gmail.com>
2022-11-16 01:20:33 +0600

    add

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const cors = require('cors');
-
-const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+const app = express();
+
+// middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qlhnchw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const appointmentCollection = client.db("doctorsPortal").collection("appointmentOptions");
+
+        // amra database a j data gulo rekhechi segulo get korlam
+        app.get('/appointmentOptions', async(req, res) => {
+           const query = {};
+           const options = await appointmentCollection.find(query).toArray();
+           res.send(options);
+        })
+      
+    } finally {
+      
+    }
+  }
+  run().catch(err=> console.log(err));
+
+
 
 app.get('/', async (req, res) => {
     res.send('Doctors portal server is running');
