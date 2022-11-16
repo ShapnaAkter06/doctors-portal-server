323b8c23b12d785f880ca3fcea971815ff331743
Shapna <shapna088@gmail.com>
2022-11-16 15:46:58 +0600

    add

--- index.js
+++ index.js
@@ -20,23 +20,35 @@ async function run() {
         const bookingsCollection = client.db("doctorsPortal").collection("bookings")
 
         // amra database a j data gulo rekhechi segulo get korlam
-        app.get('/appointmentOptions', async(req, res) => {
-           const query = {};
-           const options = await appointmentCollection.find(query).toArray();
-           res.send(options);
+        app.get('/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            const query = {};
+            const options = await appointmentCollection.find(query).toArray();
+
+            // get the booking of the provided date
+            const bookingQuery = {appointmentDate: date};
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+            
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                const bookedSlots = optionBooked.map(book => book.slot);
+                const remainingSlots = option.slots.filter(slot=> !bookedSlots.includes(slot));
+                option.slots = remainingSlots;
+            })
+            res.send(options);
         })
 
-        app.post('/bookings', async (req, res) =>{
+        app.post('/bookings', async (req, res) => {
             const booking = req.body;
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
-      
+
     } finally {
-      
+
     }
-  }
-  run().catch(err=> console.log(err));
+}
+run().catch(err => console.log(err));
 
 
 
@@ -44,6 +56,6 @@ app.get('/', async (req, res) => {
     res.send('Doctors portal server is running');
 })
 
-app.listen(port, async(req, res) => {
+app.listen(port, async (req, res) => {
     console.log(`Server is running on port ${port}`);
 })
