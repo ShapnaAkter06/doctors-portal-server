b147ae0a7d9f29ae80ec1abb31d11f9f0cb60e59
Shapna <shapna088@gmail.com>
2022-11-17 19:25:50 +0600

    add

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ async function run() {
             const email = req.query.email;
             const decodedEmail = req.decoded.email;
 
-            if( email !== decodedEmail ){
+            if (email !== decodedEmail) {
                 return res.status(403).send({ message: 'forbidden access' });
             }
             const query = { email: email };
@@ -146,9 +146,44 @@ async function run() {
             res.status(403).send({ accessToken: '' });
         })
 
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollection.findOne(query);
+
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         })
 
