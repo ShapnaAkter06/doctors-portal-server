8bf7b8a842ce3d7b3f3cd75100210a68670aeffb
Shapna <shapna088@gmail.com>
2022-11-19 19:37:05 +0600

    add

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ async function run() {
         const appointmentCollection = client.db("doctorsPortal").collection("appointmentOptions");
         const bookingsCollection = client.db("doctorsPortal").collection("bookings")
         const usersCollection = client.db("doctorsPortal").collection("users")
+        const doctorsCollection = client.db("doctorsPortal").collection("doctors")
 
         // Use Aggregate to query multiple collection and then merge data
         // amra database a j data gulo rekhechi segulo get korlam
@@ -191,6 +192,18 @@ async function run() {
             const query = {}
             const result = await appointmentCollection.find(query).project({name: 1}).toArray();
             res.send(result);
+        })
+
+        app.get('/doctors', async (req, res) => {
+            const query = {};
+            const doctors = await doctorsCollection.find(query).toArray();
+            res.send(doctors)
+        }).
+
+        app.post('/doctors', async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorsCollection.insertOne(doctor);
+            res.send(result)
         })
 
     } finally {
