b65800129e6ca43e1df7ec7b28e6524f6936c164
Shapna <shapna088@gmail.com>
2022-11-20 00:38:20 +0600

    add

--- index.js
+++ index.js
@@ -41,6 +41,12 @@ async function run() {
         const usersCollection = client.db("doctorsPortal").collection("users")
         const doctorsCollection = client.db("doctorsPortal").collection("doctors")
 
+        //make sure you use verifyAdmin after verifyJWT
+        const verifyAdmin = (req, res, next) => {
+            console.log('inside verifyAdmin', req.decoded.email);
+            next()
+        }
+
         // Use Aggregate to query multiple collection and then merge data
         // amra database a j data gulo rekhechi segulo get korlam
         app.get('/appointmentOptions', async (req, res) => {
@@ -194,19 +200,19 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/doctors', async (req, res) => {
+        app.get('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
             const query = {};
             const doctors = await doctorsCollection.find(query).toArray();
             res.send(doctors)
         })
 
-        app.post('/doctors', async (req, res) => {
+        app.post('/doctors', verifyJWT, async (req, res) => {
             const doctor = req.body;
             const result = await doctorsCollection.insertOne(doctor);
             res.send(result)
         });
 
-        app.delete('/doctors/:id', async (req, res) => {
+        app.delete('/doctors/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = {_id: ObjectId(id)};
             const result = await doctorsCollection.deleteOne(filter);
