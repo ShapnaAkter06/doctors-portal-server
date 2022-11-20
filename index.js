f77554b1cccfacdc555f07c180f05e89b04be501
Shapna <shapna088@gmail.com>
2022-11-21 00:51:33 +0600

    add

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ async function run() {
         const bookingsCollection = client.db("doctorsPortal").collection("bookings")
         const usersCollection = client.db("doctorsPortal").collection("users")
         const doctorsCollection = client.db("doctorsPortal").collection("doctors")
+        const paymentsCollection = client.db("doctorsPortal").collection("payments")
 
         //make sure you use verifyAdmin after verifyJWT
         const verifyAdmin = async (req, res, next) => {
@@ -174,6 +175,21 @@ async function run() {
             res.send({
                 clientSecret: paymentIntent.client_secret,
             });
+        })
+
+        app.post('/payments', async(req, res) => {
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId;
+            const filter = {_id: ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const updatedResult = await bookingsCollection.updateOne(filter, updateDoc);
+            res.send(result)
         })
 
         app.get('/jwt', async (req, res) => {
